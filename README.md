53c433dd66f4c13cfd37cddfa5d86b7f11dc576c
KendrickPC <kennethpchang@gmail.com>
2017-02-08 13:28:16 +0800

    feature: added levels to app.js

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ knockout.js
 [] favicon.ico retreived from the following link:
 http://www.favicon.cc/?action=icon&file_id=888553
 
+[] added levels to app.js file
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,16 +1,53 @@
 // creating a new ViewModel
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable('corgi1');
+	this.name = ko.observable('Corgi #1');
 	this.imgSrc = ko.observable('img/corgi1.jpg');
 	this.imgAttribution = ko.observable('http://www.google.com');
-
+// adding levels to my clickCount
+	this.level = ko.computed(function() {
+		if (this.clickCount() < 1) {
+			level = "Unborn puppy. Please click on picture to give birth to puppy.";
+			return level;
+		}
+		else if (this.clickCount() < 2) {
+			level = "1 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 3) {
+			level = "2 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 4) {
+			level = "3 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 5) {
+			level = "4 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 6) {
+			level = "5 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 7) {
+			level = "6 year old puppy";
+			return level;
+		}
+		else if (this.clickCount() < 8) {
+			level = "7 year old puppy";
+			return level;
+		}
+		else {
+			level = "Corgi puppy FOREVER!";
+			return level;
+		};
+	},this);
 // effectively what I have done below is the following:
 // 		var count = 0;
 // 		count++;
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-
 	};
 }
 // apply bindings in a new ViewModel
