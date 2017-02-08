342944447c0021e9049439009c1165035f379823
KendrickPC <kennethpchang@gmail.com>
2017-02-08 17:47:20 +0800

    data binding for clicks is working

--- js/app.js
+++ js/app.js
@@ -1,9 +1,48 @@
-var Dog = function () {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Corgi #1');
-	this.nickNames = ko.observableArray(["Perkins the Corgi", "Abigail the Border Collie with a Corgi sister", "Corgi Love #3", "Corgi Love #4", "Corgi Love #5"]);
-	this.imgSrc = ko.observable('img/corgi1.jpg');
-	this.imgAttribution = ko.observable('http://www.images.google.com');
+var initialDogs = [
+        {
+            clickCount : 0,
+            name : 'Corgi 1',
+            imgSrc : 'img/corgi1.jpg',
+            imgAttribution : 'https://www.images.google.com',
+            nickNames : ["Perkins the Corgi", "Abigail the Border Collie with a Corgi sister", "Corgi Love #3", "Corgi Love #4", "Corgi Love #5"]
+        },
+        {
+            clickCount : 0,
+            name : 'Corgi 2',
+            imgSrc : 'img/corgi2.jpg',
+            imgAttribution : 'https://www.images.google.com',
+            nickNames : ['Tigger']
+        },
+        {
+            clickCount : 0,
+            name : 'Corgi 3',
+            imgSrc : 'img/corgi3.jpg',
+            imgAttribution : 'https://www.images.google.com',
+            nickNames : ['Pooh']
+        },
+        {
+            clickCount : 0,
+            name : 'Corgi 4',
+            imgSrc : 'img/corgi4.jpg',
+            imgAttribution : 'https://www.images.google.com',
+            nickNames : ['Rabbit']
+        },
+        {
+            clickCount : 0,
+            name : 'Corgi 5',
+            imgSrc : 'img/corgi5.jpg',
+            imgAttribution : 'https://www.images.google.com',
+            nickNames : ['Owl']
+        }
+    ]
+// };
+
+var Dog = function (data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.nickNames = ko.observableArray(data.nickNames);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
 // adding levels to my clickCount
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 1) {
@@ -35,7 +74,7 @@ var Dog = function () {
 		}
 		else {
 			level = "Corgi puppy FOREVER!";
-		};
+		}
 		return level;
 	},this);
 }
@@ -44,15 +83,23 @@ var ViewModel = function() {
 	// wrote a function "that" to access my outer "this"
 	var that = this;
 
-	this.currentDog = ko.observable( new Dog() );
-// effectively what I have done below is the following:
-// 		var count = 0;
-// 		count++;
+	this.dogList = ko.observableArray([]);
+
+	initialDogs.forEach(function(dogItem) {
+		that.dogList.push( new Dog(dogItem)	);
+	});
+
+	this.currentDog = ko.observable(this.dogList() [0]	);
+
 	this.incrementCounter = function() {
 		// calling in the inner "this"
 		that.currentDog().clickCount(that.currentDog().clickCount() + 1);
 	};
-}
+
+	this.setDog = function() {
+		console.log('working');
+	};
+};
 // apply bindings in a new ViewModel
 ko.applyBindings(new ViewModel())
 
